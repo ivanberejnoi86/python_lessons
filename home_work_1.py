92f45294df2deacd504389d13ccf20c33f3b8aba
Ivan Berejnoi <ivanberejnoi86@gmail.com>
2022-01-19 20:34:45 +0200

    finish

---
+++ home_work_1.py
@@ -0,0 +1,31 @@
+# Create a variables
+first = 10
+second = 30
+
+# variables addition
+sum_var = first + second
+print(sum_var)
+
+# variables subtraction
+sub_var = second - first
+print(sub_var)
+
+# variables multiplication
+mult_var = first * second
+print(mult_var)
+
+# variables division
+div_var = second / first
+print(div_var)
+
+# exponential a variable
+exp_var = first ** second
+print(exp_var)
+
+# division without remainder
+without_rmeainder = second // first
+print(without_rmeainder)
+
+# remainder of the division
+remainder_div = second % first
+print(remainder_div)
