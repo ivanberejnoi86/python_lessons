9c43d91fa013b7643c1f6bf50c960514a72ff7e6
Ivan Berejnoi <ivanberejnoi86@gmail.com>
2022-01-22 23:17:30 +0200

    yap

---
+++ home_work_2.py
@@ -0,0 +1,30 @@
+#######################################
+
+print("Удвоенный результат целого числа.")
+get_value = input("Введите число:")
+transform_value = int(get_value)
+new_value = transform_value * 2
+print(f"Вы ввели {get_value}, результат- {new_value}.")
+
+########################################
+
+print("Удвоенный результат не целого числа.")
+get_value = input("Введите число:")
+transform_value = float(get_value)
+new_value = transform_value * 2
+print("Вы ввели {}, результат- {}.".format(get_value, new_value))
+
+##########################################
+
+print("Удвоеная строка.")
+get_str = input("Введите строку:")
+new_str = get_str * 2
+print("Вы ввели: " + get_str + " результат " + new_str)
+
+
+
+
+
+
+
+
