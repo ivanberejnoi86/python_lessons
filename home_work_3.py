bdb9466e065fa6cda6dec68133b05501e332692d
Ivan Berejnoi <ivanberejnoi86@gmail.com>
2022-02-02 21:26:37 +0200

    brain work4

--- home_work_3.py
+++ home_work_3.py
@@ -1,3 +1,30 @@
 #################################################################
 my_list = list(range(10, 201, 10))
+for number in my_list:
+    if number > 100:
+        print(number)
+
+##################################################################
+my_list = list(range(10, 201, 10))
+my_result = []
+for number in my_list:
+    if number > 100:
+        my_result.append(number)
+print(my_result)
+
+##################################################################
+my_list = [1, 4, 4]
+count_idx = len(my_list)
+if count_idx < 2:
+     my_list.append(0)
+elif count_idx >= 2:
+     my_list.append(my_list[count_idx-1] + my_list[count_idx-2])
 print(my_list)
+###################################################################
+my_string = '0123456789'
+my_list = []
+for smb_1 in my_string:
+     for smb_2 in my_string:
+         my_list.append(int(smb_1 + smb_2))
+print(my_list)
+####################################################################
